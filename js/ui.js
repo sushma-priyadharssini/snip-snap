0a75befa22c3844911ef6f44c42e37c7bccf6d7c
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2020-10-26 18:53:49 +0530

    initialized pwa event listeners and fixed criterias to bring up install banner

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'left'});
-  // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'right'});
+	// nav menu
+	const menus = document.querySelectorAll('.side-menu');
+	M.Sidenav.init(menus, {edge: 'left'});
+	// add recipe form
+	const forms = document.querySelectorAll('.side-form');
+	M.Sidenav.init(forms, {edge: 'right'});
+
+	// var elems = document.querySelectorAll('.fixed-action-btn');
+	// var instances = M.FloatingActionButton.init(elems, {
+    //   direction: 'right',
+    //   hoverEnabled: false
+    // });
 });
